1da4a4168bdb3e2d88078c2ffc995c85e2cb2fd3
Humma Noshine <Hummanoshine@gmail.com>
2022-04-02 14:14:01 +0100

    all links working...login backend next

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,12 +1,12 @@
 const sequelize = require('../config/connection');
-const { Signup } = require('../models/signUp');
+const { User } = require('../models/User');
 
 const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await Signup.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
