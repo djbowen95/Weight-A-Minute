8b20756aeb7b37608ef04808dd20fe3d9977bfd2
Humma Noshine <Hummanoshine@gmail.com>
2022-04-02 12:46:11 +0100

    finally main handlebar working

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+
+
+// Signup.hasMany(Project, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// Project.belongsTo(Signup, {
+//   foreignKey: 'user_id'
+// });
+
+module.exports = User;
