bc83d53d22052035b01a36b78c2867517c23b272
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-04-10 21:31:47 +0100

    exercise record to display table data/not working

--- models/ExerciseRecord.js
+++ models/ExerciseRecord.js
@@ -2,43 +2,30 @@ const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
-const ExerciseRecord = sequelize.define(
-  "ExerciseRecord",
-  {
-    // Model attributes are defined here
-    sets: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      timestamps: true,
-    },
-
-    exercise_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-
-    workout_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-
-    unit: {
-      type: DataTypes.STRING(2),
-    },
+const ExerciseRecord = sequelize.define("ExerciseRecord", {
+  // Model attributes are defined here
+  sets: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    timestamps: true,
   },
-  {
-    tableName: "exercise_record",
-  },{
-    var { sequelize } = new Sequelize('sequelize_test', 'root', null, {
-        host: "127.0.0.1",
-        dialect: 'mysql',
-        define: {
-            timestamps: false
-        }
-    });
-  }
-);
-;
+  exercise_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    timestamps: true,
+  },
+
+  workout_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    timestamps: true,
+  },
+
+  unit: {
+    type: DataTypes.STRING(2),
+    timestamps: true,
+  },
+});
 
 // `sequelize.define` also returns the model
 console.log(ExerciseRecord === sequelize.models.ExerciseRecord); // true
