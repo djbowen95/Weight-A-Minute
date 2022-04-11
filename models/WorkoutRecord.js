b0ed0fcc06fe0d912a0f58a6f4364ddeab5dfc41
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-04-11 23:40:23 +0100

    workout record model updated

---
+++ models/WorkoutRecord.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class WorkoutRecord extends Model {}
+
+WorkoutRecord.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date: {
+      type: DataTypes.STRING,
+    },
+    length: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "WorkoutRecord",
+  }
+);
+
+module.exports = WorkoutRecord;
