f3e74af7ce7b6069ee51187f2b92c2b1e3c79e4b
Daniel Bowen <djbowen95@gmail.com>
2022-04-11 21:19:31 +0100

    POST Exercise Record and Exercise Bank with one API route

--- controllers/api/exerciseRecordRoutes.js
+++
@@ -1,6 +0,0 @@
-// Write new exercise record - make sure it has all the data?
-
-// Read an exercise record:
-// Check unit against user unit (1); convert if not
-// Read all values
-// Convert all values into javascript object.
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const workoutRoutes = require('./workoutRoutes');
-const bankRoutes = require('./bankRoutes')
+const exerciseRoutes = require('./exerciseRoutes')
 
 // Workout Routes, Exercise Routes, History Routes (?)
 
 router.use('/users', userRoutes);
 router.use('/workout', workoutRoutes);
-router.use('/bank', bankRoutes);
+router.use('/exercise', exerciseRoutes);
 
 module.exports = router;
