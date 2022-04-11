8e6b523c30ea69bfe85f463660f0b6875a91e975
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-04-11 23:37:42 +0100

    updating index model file

--- models/index.js
+++ models/index.js
@@ -1,4 +1,30 @@
 const User = require("./User");
+const WorkoutRecord = require("./WorkoutRecord");
+const ExerciseBank = require("./ExerciseBank");
 const ExerciseRecord = require("./ExerciseRecord");
 
-module.exports = { User, ExerciseRecord };
+WorkoutRecord.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+ExerciseRecord.belongsTo(WorkoutRecord, {
+  foreignKey: "workout_id",
+  onDelete: "CASCADE",
+});
+
+ExerciseRecord.belongsTo(ExerciseBank, {
+  foreignKey: "bank_id",
+  onDelete: "CASCADE",
+});
+
+// Signup.hasMany(Project, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// Project.belongsTo(Signup, {
+//   foreignKey: 'user_id'
+// });
+
+module.exports = { User, WorkoutRecord, ExerciseBank, ExerciseRecord };
