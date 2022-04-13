b17d6192ebafa4b5157c2889a62d04499c722859
Humma Noshine <Hummanoshine@gmail.com>
2022-04-13 23:26:32 +0100

    images

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,9 @@
 DROP DATABASE IF EXISTS weight_a_minute;
 CREATE DATABASE weight_a_minute;
 USE weight_a_minute;
+
+
+CREATE TABLE `images` ( 
+ `id` int(10) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+ `image_name` varchar(255) DEFAULT NULL
+)
