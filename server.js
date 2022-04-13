fca866df0e34c39a930ce0b05cef9ff7b63d0317
Humma Noshine <Hummanoshine@gmail.com>
2022-04-14 00:46:19 +0100

    s3ing

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
+// import express from 'express'
+const { generateUploadURL } = require ('./s3')
+
+// {{!-- hn new --}}
 const app = express();
 const PORT = process.env.PORT || 3001;
 // handlebars stuff
@@ -28,11 +32,22 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.get('/s3Url', async (req, res) => {
+  const url = await generateUploadURL()
+    res.send({url})
+})
+
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+// app.use(express.static('front'))
 app.use(routes);
+
+
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Wahoo! App Now listening at port 3001'));
