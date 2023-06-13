cb90630caed7d6162e1e4e7598a9d2f25aaf18dd
Daniel Bowen <djbowen95@gmail.com>
2023-06-13 20:17:20 +0100

    Remove progress routes from api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,10 @@ const userRoutes = require("./userRoutes");
 const historyRoutes = require("./historyRoutes");
 const workoutRoutes = require('./workoutRoutes');
 const exerciseRoutes = require('./exerciseRoutes');
-const progressRoutes = require('./progressRoutes');
 
 router.use('/users', userRoutes);
 router.use('/workout', workoutRoutes);
 router.use('/exercise', exerciseRoutes);
 router.use("/history", historyRoutes);
-router.use('/progress', exerciseRoutes);
 
 module.exports = router;
